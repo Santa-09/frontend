a82d167657e5e32149781e3e619dc2417ae510bf
Santanu Barik <santanub957@gmail.com>
2025-08-16 19:56:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // frontend/config.js
 // Production backend URL (Railway)
-window.BACKEND_URL = "https://backend-production-cdc5.up.railway.app";
+window.BACKEND_URL = "https://chic-reprieve-production.up.railway.app";
 window.ADMIN_KEY = "santanu@2006"; 
